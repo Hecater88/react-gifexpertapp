23d1495046ffd84b4ddfb3f9a76ac0fbb736b45e
Virgilio Nayal <virgilionayal@gmail.com>
2020-08-31 20:54:02 +0200

    docs agregadp

---
+++ docs/precache-manifest.1b45f1f57e9a9a322698f8818efe97ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ea4bda38e4797163a67ff4b44b124f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "049ef881530822cbc318",
+    "url": "/static/css/main.02860dce.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "049ef881530822cbc318",
+    "url": "/static/js/main.e561f745.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
